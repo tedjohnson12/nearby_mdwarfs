d73dc73c140a8532ba4e51ad79cf9778b25549b3
Ted Johnson <ted.johnson@unlv.edu>
2024-01-18 13:14:02 -0800

    Allow more robust CLI There are now many options that can be chosen

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-<img src='nearby_mdwarfs_insolation.png'>
+<img src='nearby_exoplanets.png'>
+
+To get this figure, run:
+
+```bash
+>>> python make_figure.py
+```
+
+There are also a variety of options:
+
+```bash
+python make_figure.py -h
+
+usage: python make_figure.py [-h] [-t MAX_TEFF] [-d MAX_DIST] [-o OUTPUT] [-p MAX_PERIOD] [-m MAX_MASS] [-i MAX_INSOL]
+                             [-s SIZE] [-a ALPHA] [--mirecle]
+
+Creates a figure of nearby exoplanets.
+
+options:
+  -h, --help            show this help message and exit
+  -t MAX_TEFF, --max_teff MAX_TEFF
+                        Maximum effective temperature in K
+  -d MAX_DIST, --max_dist MAX_DIST
+                        Maximum distance in parsecs
+  -o OUTPUT, --output OUTPUT
+                        Output filename
+  -p MAX_PERIOD, --max_period MAX_PERIOD
+                        Maximum orbital period in days
+  -m MAX_MASS, --max_mass MAX_MASS
+                        Maximum planet mass in Earth masses
+  -i MAX_INSOL, --max_insol MAX_INSOL
+                        Maximum planet insolation in Earth fluxes
+  -s SIZE, --size SIZE  Marker size scale factor
+  -a ALPHA, --alpha ALPHA
+                        Transparency
+  --mirecle             Include MIRECLE target list
+
+Created by: Ted Johnson (GSFC 693) in Oct 2022, uploaded to Github 2023-04-07
+```
--- make_figure.py
+++ make_figure.py
@@ -6,138 +6,336 @@ Created by Ted Johnson (NASA GSFC 693) in Oct 2022,
 uploaded to Github 2023-04-07
 
 """
+from pathlib import Path
+from typing import Callable
+from datetime import datetime
+from io import StringIO
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import rc
 import pandas as pd
-from pathlib import Path
-from os import chdir
-from datetime import datetime
-from astropy import units as u, constants as c
 import requests
 
-def size(mass:float):
+
+def build_query(max_teff: int, max_dist: float) -> str:
     """
-    Determine marker size based on the planet's mass
+    Build the query to send to NExScI.
 
     Parameters
     ----------
-    mass : float
-        The planet's mass relative to Earth.
-    
+    max_teff : int
+        The maximum effective temperature in K
+    max_dist : float
+        The maximum distance in parsecs
+
     Returns
     -------
-    float
-        The marker size.
+    str
+        The query string
     """
-    k = 30 # scales size of markers
-    return k*mass
-
-if __name__ in '__main__':
-
-    WORKING_DIRECTORY = Path(__file__).parent
-    chdir(WORKING_DIRECTORY)
-
-    # Build the query
-    q = 'select * from pscomppars where st_teff < 3700 and sy_dist < 20'
+    q = f'select * from pscomppars where st_teff < {max_teff} and sy_dist < {max_dist}'
     query = '+'.join(q.split(' '))+'&format=csv'
+    return query
+
+
+def get_data(max_teff: int, max_dist: float) -> pd.DataFrame:
+    """
+    Get the data from NExScI.
+
+    Parameters
+    ----------
+    max_teff : int
+        The maximum effective temperature in K
+    max_dist : float
+        The maximum distance in parsecs
+
+    Returns
+    -------
+    pd.DataFrame
+        The data
+    """
+    query = build_query(max_teff, max_dist)
     url = f'https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query={query}'
     print(f'Querying {url}')
-    response = requests.get(url)
-    # write the file
-    filename = 'nearby_mdwarfs.csv'
-    with open(filename,'w') as file:
-        file.write(f'# {datetime.now().strftime("%Y/%m/%d %H:%M")}\n')
-        file.write(response.text)
-     
+    response = requests.get(url, timeout=30)
+    text = StringIO(response.text)
+    return pd.read_csv(text)
 
-    #read the data
-    df = pd.read_csv('nearby_mdwarfs.csv',comment='#')
-    # make some corrections
+
+def apply_corrections(
+    _df: pd.DataFrame,
+    max_period: float,
+    max_mass: float,
+    max_insolation: float
+) -> pd.DataFrame:
+    """
+    Apply corrections to the data.
+
+    Parameters
+    ----------
+    _df : pd.DataFrame
+        The data
+    max_period : float
+        The maximum orbital period
+    max_mass : float
+        The maximum planet mass
+    max_insolation : float
+        The maximum insolation
+
+    Returns
+    -------
+    pd.DataFrame
+        The corrected data
+    """
     # GJ 667 C radius
-    is_gj667c = df['hostname'] == 'GJ 667 C'
-    df.loc[is_gj667c,'st_rad'] = 0.42
-    df['pl_approx_insol'] = 10**(df['st_lum']) / (df['pl_orbsmax'])**2
+    is_gj667c = _df['hostname'] == 'GJ 667 C'
+    _df.loc[is_gj667c, 'st_rad'] = 0.42
+    _df['pl_approx_insol'] = 10**(_df['st_lum']) / (_df['pl_orbsmax'])**2
 
-    # get MIRECLE target list
-    # From Avi 09/2023
-    mirecle = pd.read_csv('mirecle_targets.txt',names=['name'])
+    has_all = ~np.isnan(_df['sy_dist']) & ~np.isnan(
+        _df['pl_bmasse']) & ~np.isnan(_df['pl_orbper'])
+    keep = (_df['pl_orbper'] < max_period) & has_all & (
+        _df['pl_bmasse'] < max_mass) & (_df['pl_approx_insol'] < max_insolation)
+    return _df[keep]
 
-    # make the figure
+
+def get_mirecle_targets() -> pd.DataFrame:
+    """
+    Get the MIRECLE target list.
+
+    Returns
+    -------
+    pd.DataFrame
+        The MIRECLE target list
+    """
+    path = Path(__file__).parent / 'mirecle_targets.txt'
+    mirecle_list = pd.read_csv(path, names=['name'])
+    return mirecle_list
+
+
+def print_demographics(_df):
+    """
+    Prind some demographics for Ravi.
+
+    Parameters
+    ----------
+    _df : pd.DataFrame
+        The data
+    """
+    is_transit = _df['tran_flag'].values.astype('bool')
+    n_transit = np.sum(is_transit)
+    n_nontransit = np.sum(~is_transit)
+    print(
+        f'There are {n_transit} transiting exoplanets and {n_nontransit} non-transiting exoplanets.')
+
+
+def setup_fig():
+    """
+    Set up the figure.
+    """
     plt.style.use('bmh')
     rc('font', weight='bold')
-    fig,ax = plt.subplots(1,1,figsize=(12.5,7))
-    ax.tick_params(axis='both', which='major', labelsize=14)
+    _fig, _ax = plt.subplots(1, 1, figsize=(12.5, 7))
+    _ax.tick_params(axis='both', which='major', labelsize=14)
 
-    #get date
-    with open(filename,'r') as file:
-        date = file.readline()[1:-1]
+    date = datetime.now().strftime("%Y-%m-%d")
     dist_from_bottom = 0.05
-    fig.text(0.01,dist_from_bottom,'NASA Exoplanet Archive '+date,fontfamily='serif',fontsize=14,weight='normal')
-    fig.text(0.9,dist_from_bottom, 'Created by: Ted Johnson (GSFC)',fontfamily='serif',fontsize=14,ha='right',weight='normal')
+    _fig.text(0.4, dist_from_bottom, 'NASA Exoplanet Archive ' +
+              date, fontfamily='serif', fontsize=14, weight='normal',ha='right')
+    _fig.text(0.6, dist_from_bottom, 'Created by: Ted Johnson (UNLV, GSFC)',
+              fontfamily='serif', fontsize=14, ha='left', weight='normal')
 
-    alpha=0.5 # transparency
-    k = 20 # scales size of markers
+    return _fig, _ax
 
-    has_all = ~np.isnan(df['sy_dist']) & ~np.isnan(df['pl_bmasse']) & ~np.isnan(df['pl_orbper'])
-    is_transit = df['tran_flag'].values.astype('bool')
-    keep  = (df['pl_orbper'] < 25) & has_all & (df['pl_bmasse'] < 20) & (df['pl_approx_insol'] < 100)
 
-    in_mirecle = df.loc[:,'pl_name'].isin(mirecle['name'])
+def plot(
+    _df: pd.DataFrame,
+    _ax: plt.Axes,
+    plot_mirecle: bool,
+    size_func: Callable,
+    _alpha: float
+):
+    """
+    Plot the data.
 
+    Parameters
+    ----------
+    _df : pd.DataFrame
+        The data
+    _ax : plt.Axes
+        The axes to plot on
+    plot_mirecle : bool
+        Whether to plot the MIRECLE targets
+    size_func : function
+        The function to determine the marker size
+    _alpha : float
+        The marker transparency
+    """
+    is_transit = _df['tran_flag'].values.astype('bool')
+    mirecle_target_list = get_mirecle_targets()
+    in_mirecle = _df.loc[:, 'pl_name'].isin(mirecle_target_list['name'])
     x_kw = 'sy_dist'
     y_kw = 'pl_approx_insol'
     size_kw = 'pl_bmasse'
 
-    # get some demographics for Ravi
-    within_10 = df['sy_dist'] <= 10
-    within_20 = df['sy_dist'] <= 20
-    print(f'There are {len(df.loc[~is_transit & keep & within_10])} non-transiting exoplanets within 10 pc (Porbit < 20 days, Mass < 20 Mearth, Insolation < 100 Iearth)')
-    print(f'There are {len(df.loc[~is_transit & keep & within_20])} non-transiting exoplanets within 20 pc (Porbit < 20 days, Mass < 20 Mearth, Insolation < 100 Iearth)')
-    print(f'There are {len(df.loc[is_transit & keep & within_10])} transiting exoplanets within 10 pc (Porbit < 20 days, Mass < 20 Mearth, Insolation < 100 Iearth)')
-    print(f'There are {len(df.loc[is_transit & keep & within_20])} transiting exoplanets within 20 pc (Porbit < 20 days, Mass < 20 Mearth, Insolation < 100 Iearth)')
+    _ax.scatter(_df.loc[~is_transit, x_kw], (_df.loc[~is_transit, y_kw]),
+                label='Non-Transiting', c='C0', s=size_func(_df.loc[~is_transit, size_kw]), alpha=_alpha)
+    _ax.scatter(_df.loc[is_transit, x_kw], (_df.loc[is_transit, y_kw]), label='Transiting',
+                c='C4', s=size_func(_df.loc[is_transit, size_kw]), alpha=_alpha)
+    if plot_mirecle:
+        _ax.scatter(_df.loc[in_mirecle, x_kw], _df.loc[in_mirecle, y_kw], label='MIRECLE Targets', facecolors='none',
+                    edgecolors='k', linewidth=1.5, s=size_func(_df.loc[in_mirecle, size_kw]), alpha=_alpha)
+    _ax.scatter(np.nan, -1, label='Mars-mass', c='k',
+                s=size_func(0.107), alpha=_alpha)
+    _ax.scatter(np.nan, -1, label='Earth-mass',
+                c='k', s=size_func(1), alpha=_alpha)
+    _ax.scatter(np.nan, -1, label='Neptune-mass',
+                c='k', s=size_func(17.15), alpha=_alpha)
 
-    ax.scatter(df.loc[~is_transit & keep,x_kw],(df.loc[~is_transit & keep,y_kw]),label='Non-Transiting',c='C0',s=size(df.loc[~is_transit & keep,size_kw]),alpha=alpha)
-    ax.scatter(df.loc[is_transit & keep,x_kw],(df.loc[is_transit & keep,y_kw]),label='Transiting',c='C4',s=size(df.loc[is_transit & keep,size_kw]),alpha=alpha)
-    ax.scatter(df.loc[in_mirecle & keep, x_kw],df.loc[in_mirecle & keep, y_kw],label='MIRECLE Targets',facecolors='none',edgecolors='k',linewidth=1.5,s=size(df.loc[in_mirecle & keep, size_kw]),alpha=alpha)
-    ax.scatter(np.nan,-1,label='Mars-mass',c='k',s=size(0.107),alpha=alpha)
-    ax.scatter(np.nan,-1,label='Earth-mass',c='k',s=size(1),alpha=alpha)
-    ax.scatter(np.nan,-1,label='Neptune-mass',c='k',s=size(17.15),alpha=alpha)
 
-    lw=1
-    ls=(0,(5,10))
+def add_solar_system_planets(_ax: plt.Axes):
+    """
+    Add the solar system planets to the plot.
+
+    Parameters
+    ----------
+    _ax : plt.Axes
+        The axes to plot on
+    """
+    xlo, xhi = _ax.get_xlim()
+    lw = 1
+    ls = (0, (5, 10))
     stop = 1
-    x=0.5
-    A=0.8
-    ha='left'
+    x = xlo + 0.01*(xhi-xlo)
+    text_height_scale = 0.8
+    ha = 'left'
     rot = 0
-    fontsize=13
-    alpha = 1
+    fontsize = 13
+    _alpha = 1
     color = 'xkcd:terracotta'
     zorder = -100
 
-    a_venus = 0.723 # AU
-    ax.axhline(1/a_venus**2,0,stop,c=color,lw=lw,alpha=alpha,ls=ls,zorder=zorder)
-    ax.text(x,A*1/a_venus**2,'Venus',ha=ha,rotation=rot,fontfamily='serif',fontsize=fontsize,alpha=alpha,weight='bold')
+    a_venus = 0.723  # AU
+    _ax.axhline(1/a_venus**2, 0, stop, c=color, lw=lw,
+                alpha=_alpha, ls=ls, zorder=zorder)
+    _ax.text(x, text_height_scale*1/a_venus**2, 'Venus', ha=ha, rotation=rot,
+             fontfamily='serif', fontsize=fontsize, alpha=_alpha, weight='bold')
 
-    a_mars = 1.523 # AU
-    ax.axhline(1/a_mars**2,0,stop,c=color,lw=lw,alpha=alpha,ls=ls,zorder=zorder)
-    ax.text(x,A*1/a_mars**2,'Mars',ha=ha,rotation=rot,fontfamily='serif',fontsize=fontsize,alpha=alpha,weight='bold')
+    a_mars = 1.523  # AU
+    _ax.axhline(1/a_mars**2, 0, stop, c=color, lw=lw,
+                alpha=_alpha, ls=ls, zorder=zorder)
+    _ax.text(x, text_height_scale*1/a_mars**2, 'Mars', ha=ha, rotation=rot,
+             fontfamily='serif', fontsize=fontsize, alpha=_alpha, weight='bold')
 
-    a_mercury = 0.387 # AU
-    ax.axhline(1/a_mercury**2,0,stop,c=color,lw=lw,alpha=alpha,ls=ls,zorder=zorder)
-    ax.text(x,A*1/a_mercury**2,'Mercury',ha=ha,rotation=rot,fontfamily='serif',fontsize=fontsize,alpha=alpha,weight='bold')
+    a_mercury = 0.387  # AU
+    _ax.axhline(1/a_mercury**2, 0, stop, c=color, lw=lw,
+                alpha=_alpha, ls=ls, zorder=zorder)
+    _ax.text(x, text_height_scale*1/a_mercury**2, 'Mercury', ha=ha, rotation=rot,
+             fontfamily='serif', fontsize=fontsize, alpha=_alpha, weight='bold')
 
-    ax.set_yscale('log')
+    _ax.set_yscale('log')
 
-    lgnd = ax.legend(prop={'size':14,'family':'serif'},framealpha=0.7,loc='lower right')
-    legend_marker_size = 40
-    lgnd.legendHandles[0]._sizes = [legend_marker_size]
-    lgnd.legendHandles[1]._sizes = [legend_marker_size]
 
-    ax.set_xlabel('Distance (pc)',fontfamily='serif',fontsize=14,fontweight='bold')
-    ax.set_ylabel('Stellar Insolation Flux\n(relative to Earth)',fontfamily='serif',fontsize=14,fontweight='bold')
-    # ax.set_yticks([0.5,0,5,10,50,100])
-    ax.set_title('Known Planets Around M-stars Within 20 pc',fontsize=14,fontfamily='serif',fontweight='bold')
-    fig.savefig('nearby_mdwarfs_insolation.png',dpi=120)
-    # ax.get_yticks()
+def add_legend(_ax: plt.Axes, size_func: Callable):
+    """
+    Add the legend.
+
+    Parameters
+    ----------
+    _ax : plt.Axes
+        The axes to plot on
+    size_func : function
+        The function to determine the marker size
+    """
+    lgnd = _ax.legend(prop={'size': 14, 'family': 'serif'},
+                      framealpha=0.7, loc='lower right')
+    legend_marker_size = size_func(2)
+    lgnd.legend_handles[0]._sizes = [legend_marker_size]
+    lgnd.legend_handles[1]._sizes = [legend_marker_size]
+
+
+def add_labels(_ax: plt.Axes,max_dist: float):
+    """
+    Add the labels.
+
+    Parameters
+    ----------
+    _ax : plt.Axes
+        The axes to plot on
+    max_dist : float
+    """
+    _ax.set_xlabel('Distance (pc)', fontfamily='serif',
+                   fontsize=14, fontweight='bold')
+    _ax.set_ylabel('Stellar Insolation Flux\n(relative to Earth)',
+                   fontfamily='serif', fontsize=14, fontweight='bold')
+    _ax.set_title(f'Known Planets Within {max_dist:g} pc',
+                  fontsize=14, fontfamily='serif', fontweight='bold')
+
+
+if __name__ in '__main__':
+    parser = argparse.ArgumentParser(
+        prog='python make_figure.py',
+        description='Creates a figure of nearby exoplanets.',
+        epilog='Created by: Ted Johnson (GSFC 693) in Oct 2022, uploaded to Github 2023-04-07'
+    )
+
+    parser.add_argument('-t', '--max_teff', type=int,
+                        default=3700, help='Maximum effective temperature in K')
+    parser.add_argument('-d', '--max_dist', type=float,
+                        default=20, help='Maximum distance in parsecs')
+    parser.add_argument('-o', '--output', type=str,
+                        default='nearby_exoplanets.png', help='Output filename')
+    parser.add_argument('-p', '--max_period', type=float,
+                        default=25, help='Maximum orbital period in days')
+    parser.add_argument('-m', '--max_mass', type=float,
+                        default=20, help='Maximum planet mass in Earth masses')
+    parser.add_argument('-i', '--max_insol', type=float, default=100,
+                        help='Maximum planet insolation in Earth fluxes')
+    parser.add_argument('-s', '--size', type=float,
+                        default=1, help='Marker size scale factor')
+    parser.add_argument('-a', '--alpha', type=float,
+                        default=0.5, help='Transparency')
+    parser.add_argument('--mirecle', action='store_true',
+                        help='Include MIRECLE target list')
+
+    args = parser.parse_args()
+
+    df = get_data(args.max_teff, args.max_dist)
+
+    df = apply_corrections(
+        df,
+        args.max_period,
+        args.max_mass,
+        args.max_insol
+    )
+
+    print_demographics(df)
+
+    fig, ax = setup_fig()
+
+    alpha = args.alpha  # transparency
+
+    def size(mass: float):
+        """
+        Determine marker size based on the planet's mass
+
+        Parameters
+        ----------
+        mass : float
+            The planet's mass relative to Earth.
+
+        Returns
+        -------
+        float
+            The marker size.
+        """
+        k = 30*args.size  # scales size of markers
+        return k*mass
+
+    plot(df, ax, args.mirecle, size, alpha)
+    add_solar_system_planets(ax)
+    add_legend(ax, size)
+    add_labels(ax, args.max_dist)
+
+    fig.savefig(args.output, dpi=120)
